e679ab03467d54594e82f50c3f10f357944eb25f
Robert Beal <robertbeal@gmail.com>
2015-10-30 11:27:13 +0000

    Latest updates to the script

--- install.sh
+++ install.sh
@@ -2,47 +2,64 @@
 
 # dropbox
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E &&
-sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) main" &&
-# apt-fast
-sudo add-apt-repository ppa:apt-fast/stable &&
+sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu trusty main" &&
 # android
 sudo add-apt-repository ppa:nilarimogard/webupd8 &&
 sudo add-apt-repository ppa:upubuntu-com/sdk &&
 # chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
+# atom
+sudo add-apt-repository ppa:webupd8team/atom &&
 
 sudo apt-get update &&
-sudo apt-get install -y apt-fast &&
-
-# remove
-sudo apt-fast remove -y rhythmbox tomboy gwibber thunderbird evolution nautilus-sendto ubuntuone-client empathy firefox unity-webapps-* &&
-sudo apt-fast autoremove -y &&
 
 # codecs
-sudo apt-fast install -y ubuntu-restricted-extras flashplugin-installer mencoder &&
+sudo apt-get install -y ubuntu-restricted-extras flashplugin-installer &&
 
 # internet
-sudo apt-fast install -y dropbox google-chrome-stable network-manager-vpnc &&
-
-# fs
-sudo apt-fast install -y ssh samba gparted dropbox &&
+sudo apt-get install -y dropbox google-chrome-stable network-manager-vpnc network-manager-vpnc-gnome rdesktop &&
 
 # system
-sudo apt-fast install -y synaptic rar p7zip-full gnome-system-tools unity-tweak-tool libappindicator1 eyed3 &&
+sudo apt-get install -y gparted rar p7zip-full gnome-system-tools libappindicator1 eyed3 &&
  
 # media
-sudo apt-fast install -y mp3gain flickrbackup && 
+sudo apt-get install -y vlc mp3gain && 
 
 # dev
-sudo apt-fast install -y gedit curl git virtualbox android-tools-adb android-tools-fastboot
+sudo apt-get install -y vim tmux curl git android-tools-adb android-tools-fastboot atom &&
+
+# vpn
+sudo apt-get install -y network-manager-openvpn &&
+sudo wget -O /etc/openvpn/ca.vyprvpn.com.crt https://www.goldenfrog.com/downloads/ca.vyprvpn.com.crt &&
+
+# remove
+sudo apt-get remove -y rhythmbox evolution empathy firefox unity-webapps-* apport xterm &&
+sudo apt-get autoremove -y &&
+
+# flickr
+sudo apt-get install python-pip &&
+easy_install -U pip &&
+sudo pip install requests[security]
+sudo pip install flickrbackup &&
+
+# privacy
+gsettings set com.canonical.Unity.Lenses remote-content-search none &&
+gsettings set com.canonical.Unity.Lenses disabled-scopes \
+      "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope',
+      'more_suggestions-populartracks.scope', 'music-musicstore.scope',
+      'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope',
+      'more_suggestions-skimlinks.scope']" &&
+if ! grep -q productsearch.ubuntu.com /etc/hosts; then
+	echo -e "\n127.0.0.1 productsearch.ubuntu.com" | sudo tee -a /etc/hosts >/dev/null
+fi &&
 
 # aliases
 touch .bash_aliases &&
 rm .bash_aliases &&
-echo "alias install='sudo apt-fast install'" >> .bash_aliases &&
-echo "alias remove='sudo apt-fast remove'" >> .bash_aliases &&
-echo "alias update='sudo apt-fast update'" >> .bash_aliases &&
+echo "alias install='sudo apt-get install'" >> .bash_aliases &&
+echo "alias remove='sudo apt-get remove'" >> .bash_aliases &&
+echo "alias update='sudo apt-get update'" >> .bash_aliases &&
 echo "alias ..='cd ..'" >> .bash_aliases &&
 echo "alias ...='cd ../../'" >> .bash_aliases &&
 echo "alias shutdown='sudo shutdown –h now'" >> .bash_aliases && 
@@ -54,11 +71,12 @@ echo "alias gpp='git pull --rebase && git push'" >> .bash_aliases &&
 echo "alias gspps='git stash && git pull --rebase && git push && git stash pop'" >> .bash_aliases &&
 
 # virtualbox
-VBoxManage setextradata global GUI/Customizations noMenuBar,noStatusBar &&
-sudo usermod -aG vboxusers $USER &&
+# sudo apt-get install -y virtualbox &&
+# VBoxManage setextradata global GUI/Customizations noMenuBar,noStatusBar &&
+# sudo usermod -aG vboxusers $USER &&
 
 # vcs details
-EMAIL="example@example.com" &&
+EMAIL="you@email.com" &&
 NAME="Your Name" &&
 
 # git
@@ -70,38 +88,13 @@ git config --global alias.a add &&
 git config --global alias.d diff &&
 git config --global push.default simple &&
 
-# hg
-touch .hgrc &&
-rm .hgrc &&
-echo "[ui]" >> .hgrc &&
-echo "username = $NAME <$EMAIL>" >> .hgrc &&
-
-# ruby
-\curl -sSL https://get.rvm.io | bash -s stable --ruby &&
-source ~/.rvm/scripts/rvm &&
-
-# samba
-sudo cp $HOME/Install/Settings/smb.conf /etc/samba/smb.conf &&
-sudo chmod 664 /etc/samba/smb.conf &&
-sudo restart smbd &&
-sudo smbpasswd -L -a $USER &&
-sudo smbpasswd -L -e $USER &&
-
 # swap
 if [ -n "grep 'sysctl -w vm.swappiness' /etc/rc.local" ]; then
 	sudo sed -i 's/exit 0/sysctl -w vm.swappiness=0\nexit 0/g' /etc/rc.local
 fi &&
 
-# bash
-if [ -n "grep --quiet 'source ~/.rvm/scripts/rvm' ~/.bashrc" ]; then
-	echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc &&
-fi &&
-
 # hosts
-if [ -n "grep '10.0.0.1 router' /etc/hosts" ]; then
-	echo -e "10.0.0.1 router" | sudo tee -a /etc/hosts
-fi &&
-if [ -n "grep '10.0.0.2 xbmc' /etc/hosts" ]; then
-	echo -e "10.0.0.2 xbmc"   | sudo tee -a /etc/hosts;
+if [ -n "grep '192.168.0.1 router' /etc/hosts" ]; then
+	echo -e "192.168.0.1 router" | sudo tee -a /etc/hosts
 fi
 
