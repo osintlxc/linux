2561a7f0048b0e860bb0567f22104dc2bea7d8a8
Robert Beal <robertbeal@gmail.com>
2016-08-01 12:59:41 +0100

    Add some vim config and set up

---
+++ vim.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/wookiehangover/jshint.vim.git ~/.vim/bundle/jshint.vim
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/SuperTab
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/Syntastic
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/The-NERD-tree
