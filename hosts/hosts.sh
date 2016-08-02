dd4482d1034384da06915b842833a1d3089b7d5c
Robert Beal <rob.beal@7digital.com>
2016-08-02 17:00:13 +0100

    Update links in some of the scripts

--- hosts/hosts.sh
+++ hosts/hosts.sh
@@ -4,6 +4,6 @@ if [ ! -f /etc/hosts.original ]; then
     cp /etc/hosts /etc/hosts.original
 fi
 
-wget https://hosts.local -O /etc/hosts.local
+wget https://raw.githubusercontent.com/robertbeal/linux/master/hosts/hosts.local -O /etc/hosts.local
 wget http://winhelp2002.mvps.org/hosts.txt -O /etc/hosts.blocklist
 cat /etc/hosts.original /etc/hosts.local /etc/hosts.blocklist > /etc/hosts
--- pi/couchpotato.sh
+++ pi/couchpotato.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 sudo git clone git://github.com/RuudBurger/CouchPotatoServer.git /usr/local/couchpotato && 
-sudo wget -O /etc/init.d/couchpotato https:///couchpotato &&
+sudo wget -O /etc/init.d/couchpotato https://raw.githubusercontent.com/robertbeal/linux/master/pi/couchpotato/couchpotato &&
 sudo chmod 644 /etc/init.d/couchpotato &&
 sudo update-rc.d couchpotato defaults &&
 sudo service couchpotato start
